c4d7e6b8c932afdd38594b0cfd6d0d481aab6d01
cpnat <cpnattrass@googlemail.com>
2023-12-17 21:49:25 +0100

    Add EJS views

--- express/api.ts
+++ express/api.ts
@@ -1,10 +1,18 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import ejs from "ejs";
+import path from "path";
 
 const api = express();
-
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.set("view engine", "ejs");
+api.set("views", path.join(__dirname, "views")); // Assuming your EJS files are in a 'views' folder
+
+router.get("/hello", (req, res) => {
+  // Render the "hello.ejs" file in the views folder
+  res.render("hello", { message: "Hello World!" });
+});
 
 api.use("/api/", router);
 
